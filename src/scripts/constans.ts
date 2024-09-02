472428174f942f67e2c6f14f83f212f0f030507a
Sergey Gusterev <copywriter-sg@yandex.ru>
2024-09-03 00:31:00 +0300

    feat: mobile version

---
+++ src/scripts/constans.ts
@@ -0,0 +1,15 @@
+import heroImageLight from '../assets/images/dashboard-light.webp';
+import heroImageDark from '../assets/images/dashboard-dark.webp';
+
+import cardPrime1 from '../assets/images/card-prime-1.webp';
+import cardPrime2 from '../assets/images/card-prime-2.webp';
+import cardPrime3 from '../assets/images/card-prime-3.webp';
+
+import cardPrime1Light from '../assets/images/card-prime-light-1.webp';
+import cardPrime2Light from '../assets/images/card-prime-light-2.webp';
+import cardPrime3Light from '../assets/images/card-prime-light-3.webp';
+
+const cardsLight = [cardPrime1Light, cardPrime2Light, cardPrime3Light];
+const cardsDark = [cardPrime1, cardPrime2, cardPrime3];
+
+export { heroImageLight, heroImageDark, cardsLight, cardsDark };
--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,23 +1,18 @@
 import 'normalize.css';
 import '../styles/main.css';
-import heroImageLight from '../assets/images/dashboard-light.webp';
-import heroImageDark from '../assets/images/dashboard-dark.webp';
 
-import cardPrime1 from '../assets/images/card-prime-1.webp';
-import cardPrime2 from '../assets/images/card-prime-2.webp';
-import cardPrime3 from '../assets/images/card-prime-3.webp';
-
-import cardPrime1Light from '../assets/images/card-prime-light-1.png';
-import cardPrime2Light from '../assets/images/card-prime-light-2.png';
-import cardPrime3Light from '../assets/images/card-prime-light-3.png';
-
-const cardsLight = [cardPrime1Light, cardPrime2Light, cardPrime3Light];
-const cardsDark = [cardPrime1, cardPrime2, cardPrime3];
+import {
+  heroImageDark,
+  heroImageLight,
+  cardsDark,
+  cardsLight,
+} from './constans';
 
 const themeToggleBtn = document.getElementById('theme-toggle');
 const heroImage = document.querySelector(
   '.hero__image img',
 ) as HTMLImageElement;
+const savedTheme = localStorage.getItem('theme');
 
 function setHeroImage(theme: string) {
   if (theme === 'dark') {
@@ -56,8 +51,6 @@ function toggleTheme() {
   setHeroImage(newTheme);
   setCards(newTheme);
 }
-
-const savedTheme = localStorage.getItem('theme');
 
 if (savedTheme) {
   setTheme(savedTheme);
